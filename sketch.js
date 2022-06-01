e2850136b9c20bb768e530a168b6072334d59f71
AaronChee1 <102646608+AaronChee1@users.noreply.github.com>
2022-06-01 17:06:16 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,94 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+let engine, world, backgroundImg;
+let canvas, angle, tower, ground, cannon;
+let cannonBall;
+
+let balls=[]
+//arrays
+//array with same data type
+// let arr1 = [1,2,3]
+// console.log(arr1)
+
+// //arrays with different data types
+// let arr2 = [1,2,3, false, "Hello", null]
+// console.log(arr2)
+
+// //arrays within arrays or multidimensional arrays
+// let arr3 = [[1,2,3], [67,976,907,5647],[5,87]]
+// console.log(arr3)
+// //console.log(arr3[2][1])
+// arr3.push('Aaron')
+// console.log(arr3)
+
+// arr3.pop()
+// console.log(arr3)
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+  cannon = new Cannon(180, 110, 130, 100, angle);
+ 
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+
+  push();
+  fill("brown");
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  for(let i=0;i<balls.length; i++){
+    showCannonBalls(balls[i],i)
+  }
+  
+  cannon.display();
+ 
+}
+
+function showCannonBalls(ball,i){
+  if(ball){
+    ball.display()
+  }
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length-1].shoot();
+  }
+}
+function keyPressed(){
+  if(keyCode === DOWN_ARROW){
+    cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall)
+  }
+}
